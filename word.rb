68fdcfdcce359a905d0cccc4b0a8d1ac2803c302
jmurrel1 <jmurrel1@cs6242-vm03.uwg.westga.edu>
2021-01-19 16:14:25 -0500

    word written, untested

--- word.rb
+++ word.rb
@@ -1,9 +1,15 @@
+require "tile_bag.rb"
+
 module RubyBabble
-  class TileGroup
+  class Word < TileGroup
     def initialize
+      super()
     end
 
     def score
+      score = 0
+      @tiles.each {|tile| score += TileBag.points_for(tile)}
+      score
     end
   end
 end
