2e1f24f5ff274d93aa7b3a13c0ff73ea5660e81c
jmurrel1 <jmurrel1@cs6242-vm06.uwg.westga.edu>
2021-01-21 14:45:10 -0500

    test_score passes

--- tests/word/test_score.rb
+++ tests/word/test_score.rb
@@ -8,14 +8,27 @@ class TestInitalize < Minitest::Test
   end
 
   def test_empty_word_should_have_score_of_zero
+    assert_equal 0, @word.score
   end
 
   def test_score_a_one_tile_word
+    @word.append(:A)
+    assert_equal 1, @word.score
   end
 
   def test_score_a_word_with_multiple_different_tiles
+    @word.append(:A)
+    @word.append(:X)
+    @word.append(:E)
+    assert_equal 10, @word.score
   end
 
   def test_score_a_word_with_recurring_tiles
+    @word.append(:A)
+    @word.append(:P)
+    @word.append(:P)
+    @word.append(:L)
+    @word.append(:E)
+    assert_equal 9, @word.score
   end
 end
