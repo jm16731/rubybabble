f70bfdb3d06450c4bd5d9a035870acd9c9772fc9
jmurrel1 <jmurrel1@cs6242-vm06.uwg.westga.edu>
2021-01-21 14:32:30 -0500

    test_hand passes

--- tests/tile_group/test_hand.rb
+++ tests/tile_group/test_hand.rb
@@ -8,14 +8,29 @@ class TestHand < MiniTest::Test
   end
 
   def test_convert_empty_group_to_string
+    assert_equal "", @tile_group.hand
   end
 
   def test_convert_single_tile_group_to_string
+    @tile_group.append(:A)
+    assert_equal "A", @tile_group.hand
   end
 
   def test_convert_multi_tile_group_to_string
+    @tile_group.append(:A)
+    @tile_group.append(:B)
+    @tile_group.append(:C)
+    @tile_group.append(:D)
+    @tile_group.append(:E)
+    assert_equal "ABCDE", @tile_group.hand
   end
 
   def test_convert_multi_tile_group_with_duplicates_to_string
+    @tile_group.append(:F)
+    @tile_group.append(:U)
+    @tile_group.append(:N)
+    @tile_group.append(:N)
+    @tile_group.append(:Y)
+    assert_equal "FUNNY", @tile_group.hand
   end
 end
