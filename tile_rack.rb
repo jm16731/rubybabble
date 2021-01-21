e22b9b13f5a5172d3ed59e5d13e458f54ae70f02
jmurrel1 <jmurrel1@cs6242-vm06.uwg.westga.edu>
2021-01-21 14:48:57 -0500

    tile_rack structure in place

---
+++ tile_rack.rb
@@ -0,0 +1,16 @@
+module RubyBabble
+  class TileRack < TileGroup
+    def initialize
+      super()
+    end
+
+    def number_of_tiles_needed
+    end
+
+    def has_tiles_for?(text)
+    end
+
+    def remove_word(text)
+    end
+  end
+end
