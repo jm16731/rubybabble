39473f0ef82ac18c2e2b2d98840c9209032f8ef1
jmurrel1 <jmurrel1@cs6242-vm01.uwg.westga.edu>
2021-01-23 10:33:03 -0500

    babble.rb done, ready to test

--- babble.rb
+++ babble.rb
@@ -4,10 +4,48 @@ require "tempfile"
 module RubyBabble
   class Babble
     def intialize
+      @tile_bag = TileBag.new
+      @tile_rack = TileRack.new
+      @word = Word.new
+      @score = 0
+      @guess = ""
     end
 
     def run
+      puts "Welcome to RubyBabble"
+      until @guess == ":quit" do
+        populate_tile_rack
+        puts @tile_rack.hand
+        puts "Please guess a word using the above letters"
+        @guess = gets
+        if @guess == ":quit"
+          puts "Thanks for playing, " + get_score
+          next
+        else if not Spellchecker::check(@guess)[0][:correct]
+          puts "Not a valid word"
+        else if not @tile_rack.has_tiles_for?(@guess)
+          puts "Not enough tiles"
+        else
+          @word = @tile_rack.remove_word(@gues)
+          @score += @word.score
+          puts "You made " + @word + " for " + @word.score + " points"
+        end
+        get_score
+      end
     end
+
+    private
+
+    def populate_tile_rack
+      until @tile_rack.number_of_tiles_needed == 0 do
+        @tile_rack.append.@tile_bag.draw_tile
+      end
+    end
+
+    def get_score
+      "total score: " + @score
+    end
+
   end
 end
 
